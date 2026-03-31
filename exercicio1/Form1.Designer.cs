1b58a23981abcc5f807d9f4b3cb873a305a25678
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-31 16:10:18 -0300

    Add files via upload

---
+++ exercicio1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textnota1 = new System.Windows.Forms.TextBox();
+            this.textnota2 = new System.Windows.Forms.TextBox();
+            this.textnota3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.calcbutton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textnota1
+            // 
+            this.textnota1.Location = new System.Drawing.Point(45, 77);
+            this.textnota1.Name = "textnota1";
+            this.textnota1.Size = new System.Drawing.Size(100, 20);
+            this.textnota1.TabIndex = 0;
+            // 
+            // textnota2
+            // 
+            this.textnota2.Location = new System.Drawing.Point(45, 158);
+            this.textnota2.Name = "textnota2";
+            this.textnota2.Size = new System.Drawing.Size(100, 20);
+            this.textnota2.TabIndex = 1;
+            // 
+            // textnota3
+            // 
+            this.textnota3.Location = new System.Drawing.Point(45, 241);
+            this.textnota3.Name = "textnota3";
+            this.textnota3.Size = new System.Drawing.Size(100, 20);
+            this.textnota3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nota 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nota 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nota 3:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(45, 321);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Seu nome:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(482, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 18);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(226, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(314, 32);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "CALCULO DE MÉDIA";
+            // 
+            // calcbutton
+            // 
+            this.calcbutton.Location = new System.Drawing.Point(45, 347);
+            this.calcbutton.Name = "calcbutton";
+            this.calcbutton.Size = new System.Drawing.Size(75, 23);
+            this.calcbutton.TabIndex = 10;
+            this.calcbutton.Text = "Calcule";
+            this.calcbutton.UseVisualStyleBackColor = true;
+            this.calcbutton.Click += new System.EventHandler(this.calcbutton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(482, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 18);
+            this.label7.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.calcbutton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textnota3);
+            this.Controls.Add(this.textnota2);
+            this.Controls.Add(this.textnota1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textnota1;
+        private System.Windows.Forms.TextBox textnota2;
+        private System.Windows.Forms.TextBox textnota3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button calcbutton;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ exercicio1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        double nota1, nota2, nota3, resultado;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcbutton_Click(object sender, EventArgs e)
+        {
+            string nomedoaluno = textBox4.Text;
+
+            nota1 = double.Parse(textnota1.Text);
+            nota2 = double.Parse(textnota2.Text);
+            nota3 = double.Parse(textnota3.Text);
+
+            resultado = (nota1 + nota2 + nota3) / 3;
+
+            label7.Text = "Olá " + nomedoaluno;
+            label5.Text = "A sua média foi de: " + resultado.ToString("0.00");
+
+
+        }
+    }
+}
