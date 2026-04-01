b131b9160cc767e8819c19ad5492710e20ea2173
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-04-01 14:56:40 -0300

    Add files via upload

---
+++ projeto2/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace projeto_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idadeemhoras = new System.Windows.Forms.Label();
+            this.idadeemdias = new System.Windows.Forms.Label();
+            this.idadeemsemanas = new System.Windows.Forms.Label();
+            this.idadeemmeses = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(242, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 33);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(241, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 33);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(44, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insira seu nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(44, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insira sua idade:";
+            // 
+            // idadeemhoras
+            // 
+            this.idadeemhoras.AutoSize = true;
+            this.idadeemhoras.BackColor = System.Drawing.Color.Transparent;
+            this.idadeemhoras.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idadeemhoras.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.idadeemhoras.Location = new System.Drawing.Point(502, 237);
+            this.idadeemhoras.Name = "idadeemhoras";
+            this.idadeemhoras.Size = new System.Drawing.Size(0, 25);
+            this.idadeemhoras.TabIndex = 4;
+            // 
+            // idadeemdias
+            // 
+            this.idadeemdias.AutoSize = true;
+            this.idadeemdias.BackColor = System.Drawing.Color.Transparent;
+            this.idadeemdias.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idadeemdias.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.idadeemdias.Location = new System.Drawing.Point(502, 212);
+            this.idadeemdias.Name = "idadeemdias";
+            this.idadeemdias.Size = new System.Drawing.Size(0, 25);
+            this.idadeemdias.TabIndex = 5;
+            // 
+            // idadeemsemanas
+            // 
+            this.idadeemsemanas.AutoSize = true;
+            this.idadeemsemanas.BackColor = System.Drawing.Color.Transparent;
+            this.idadeemsemanas.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idadeemsemanas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.idadeemsemanas.Location = new System.Drawing.Point(502, 187);
+            this.idadeemsemanas.Name = "idadeemsemanas";
+            this.idadeemsemanas.Size = new System.Drawing.Size(0, 25);
+            this.idadeemsemanas.TabIndex = 6;
+            // 
+            // idadeemmeses
+            // 
+            this.idadeemmeses.AutoSize = true;
+            this.idadeemmeses.BackColor = System.Drawing.Color.Transparent;
+            this.idadeemmeses.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idadeemmeses.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.idadeemmeses.Location = new System.Drawing.Point(502, 162);
+            this.idadeemmeses.Name = "idadeemmeses";
+            this.idadeemmeses.Size = new System.Drawing.Size(0, 25);
+            this.idadeemmeses.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(49, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Tw Cen MT Condensed", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(41, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 44);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Calculadora de Idades";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(488, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.idadeemmeses);
+            this.Controls.Add(this.idadeemsemanas);
+            this.Controls.Add(this.idadeemdias);
+            this.Controls.Add(this.idadeemhoras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label idadeemhoras;
+        private System.Windows.Forms.Label idadeemdias;
+        private System.Windows.Forms.Label idadeemsemanas;
+        private System.Windows.Forms.Label idadeemmeses;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ projeto2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_2
+{
+    public partial class Form1 : Form
+    {
+        double idade;
+        string nome;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            idade = double.Parse(textBox2.Text);
+            nome = textBox1.Text;
+
+            label4.Text = "Olá "+ nome + "!";
+            idadeemmeses.Text = "Você têm " + idade * 12 + " Meses";
+            idadeemsemanas.Text = "Você têm " + idade * 48 + " Semanas";
+            idadeemdias.Text = "Você têm " + idade * 365 + " Dias";
+            idadeemhoras.Text = "Você têm " + idade * 8766 + " Horas";
+
+        }
+    }
+}
